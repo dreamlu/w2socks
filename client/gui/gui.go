b7d805237a191b7dd9fa124cf5d946851ab0c3e1
dreamlu <3147460271@qq.com>
2020-07-31 14:57:39 +0800

    主题色修改; 列表下拉完成

--- client/gui/gui.go
+++ client/gui/gui.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
-	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/layout"
+	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 	"github.com/dreamlu/w2socks/client/data"
-	"image/color"
 )
 
 var (
@@ -19,33 +18,31 @@ var (
 func Gui() fyne.Window {
 	// 主程序
 	majorApp := app.New()
+	// light theme
+	majorApp.Settings().SetTheme(theme.LightTheme())
 	// logo
 	majorApp.SetIcon(data.Logo())
 	addWindow := majorApp.NewWindow("w2socks")
 	addWindow.Resize(fyne.NewSize(280, 300))
 
-	// 连接信息列表
-	infos := widget.NewVBox()
-	//infos.Resize(fyne.NewSize(250, 200))
-
-	conf := data.GetConfig()
-	for _, v := range conf {
-		infos.Children = append(infos.Children, canvas.NewLine(color.White))
-		text := NewSelectClickText(fmt.Sprintf("%s [%s,%s]", v.Name, v.ServerIpAddr, v.LocalPort), *v)
-		infos.Children = append(infos.Children, text)
-	}
-	infos.Children = append(infos.Children, canvas.NewLine(color.White))
-
-	// 纵向列表
-	list := widget.NewScrollContainer(infos)
-	list.Resize(fyne.NewSize(250, 200))
-
 	// 主菜单
 	addWindow.SetMainMenu(MainMenu())
 	// 布局
 	//top := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), Toolbar())
-	bom := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), list.Content)
-	//oth := fyne.NewContainerWithLayout(layout.NewVBoxLayout())
-	addWindow.SetContent(fyne.NewContainerWithLayout(layout.NewVBoxLayout(), bom))
+	//bom := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), list.Content)
+	vert := widget.NewVScrollContainer(widget.NewVBox(mainList()...))
+	addWindow.SetContent(fyne.NewContainerWithLayout(layout.NewAdaptiveGridLayout(1), vert))
 	return addWindow
+}
+
+// 列表核心
+func mainList() []fyne.CanvasObject {
+	var items []fyne.CanvasObject
+
+	conf := data.GetConfig()
+	for _, v := range conf {
+		text := NewSelectClickText(fmt.Sprintf("[%s] %s", v.Name, v.ServerIpAddr), *v)
+		items = append(items, text)
+	}
+	return items
 }
